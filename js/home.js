bc2cce8a0abf8c31590e4c80bb3aad02196d00a1
Noman <nomanmia128@gmail.com>
2024-09-19 20:35:25 +0600

    added newbalence

--- js/home.js
+++ js/home.js
@@ -2,7 +2,10 @@
 /*
  s-1: add evet handler
  prevent page reload after submit
- s-2: 
+ s-2: get money to be added to the account balanse
+ get the pin number
+
+ s-3: verify the pin number
  */
 
 // step-1: add an event handler to the add money button inside the from
@@ -16,5 +19,29 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
 
     // get the pin number provided
     const pinNumberInput = document.getElementById('input-pin-number').value;
-    console.log(pinNumberInput)
+    console.log(pinNumberInput);
+
+    // step-3: verify pin number
+    // worong way to validate pin number
+    if(pinNumberInput === '1234'){
+        console.log('adding money to your account');
+
+        // step-4: get the current balance
+        const balance = document.getElementById('acount-balance').innerText;
+        console.log(balance)
+
+        // step-5: add addMoneyInput with balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+
+        // step-6: update the balance in the UI/DOM
+        document.getElementById('acount-balance').innerText = newBalance;
+
+    }
+    else{
+        alert('Falied to add money! please try agin.')
+    }
+
 })
